ff9a183c2401fbb43f0f1b8c313785f33260394f
tach_07 <mathisvoisine445@gmail.com>
2024-09-08 16:53:59 +0200

    Operateurs change

--- operateurs/src/main.rs
+++ operateurs/src/main.rs
@@ -16,4 +16,31 @@ fn main() {
     // 17 / 5 = 3 il reste 2
     // 17 % 5 = 2 <----- le reste 
 
+    // [ opérateurs ]
+
+    // bool : true/false
+    // == > >= < <= !=
+
+    let i: i8 = 5;
+    let j: i8 = 10;
+    println!("{}", i != j); //true
+    println!("{}", i == j); // false
+    println!("{}", i >= j); // false
+    println!("{}", i > j); // false
+    println!("{}", i < j); // true
+    println!("{}", i <= j); // true
+
+    println!("{}", "abc" > "abcd"); // false
+
+    // impossible de comparer deux types différents 
+    // println!("{}", 3 > 3.14); -> error
+
+    let mut z: i8 = 11;
+    z += 1;
+    println!("{}", z); // affiche 12
+    z -= 4; // 8
+    z *= 2; // 16
+    z /= 2; // 8
+    println!("{}", z); // affiche 8
+
 }
